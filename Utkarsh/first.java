c79626325919679a9b160f78bdc8c512f3a8f57d
flowbreaker <vdnuti@gmail.com>
2022-10-16 23:39:00 +0530

    This thing tells about sum of two numbers with address of my residence attached in it

---
+++ Utkarsh/first.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class first
+{
+public static void main(String arr[])
+{
+Scanner f1=new Scanner(System.in);
+ int a,b,c;
+System.out.println("Enter any Two Numbers : ");
+ a=f1.nextInt();
+ b=f1.nextInt();
+c=a+b;
+ System.out.println("Sum of Two Numbers ="+c);
+ System.out.println("Utkarsh Shukla\n");
+ System.out.println("Gulab Nagar Janaura Ayodhya");
+}
+}
